cb5d36daeab4a40424e6f29fb3f337c6d9f68fa5
Elhelw, Mohamed <114217025+melhelow@users.noreply.github.com>
2025-07-23 04:33:42 +0000

    generate devise user , run db migrate, and draft resource ,migrate again

---
+++ app/models/delivery.rb
@@ -0,0 +1,25 @@
+# == Schema Information
+#
+# Table name: deliveries
+#
+#  id          :bigint           not null, primary key
+#  description :string
+#  expected_on :date
+#  received    :boolean
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  user_id     :bigint           not null
+#
+# Indexes
+#
+#  index_deliveries_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
+class Delivery < ApplicationRecord
+  belongs_to :user
+validates :description, presence: true
+validates :expected_on, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2025_07_23_042348) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "deliveries", force: :cascade do |t|
+    t.string "description"
+    t.date "expected_on"
+    t.boolean "received"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_deliveries_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "deliveries", "users"
 end
